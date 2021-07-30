bf96147e4dbf8399646e35b96d685469c0306ff9
Chey Rose <cheyroseflammer@gmail.com>
2021-07-29 18:57:38 -0700

    register alert added

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 def register(request):
   if request.method == 'POST':
-    # Register user
-    return
+    messages.error(request, 'Testing error message')
+    return redirect('register')
   else:
     return render(request, 'accounts/register.html')
 
@@ -19,5 +20,3 @@ def logout(request):
 
 def dashboard(request):
   return render(request, 'accounts/dashboard.html')
-
-
